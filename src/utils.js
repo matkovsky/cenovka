078861965e68a24f94ee744de1caf70c12be57eb
Peter Matkovsky <peter@kuix.co>
2020-05-19 10:31:10 +0200

    Update fraction digits to be exactly 2 when formatting price

--- src/utils.js
+++ src/utils.js
@@ -2,4 +2,4 @@ export const normalizePrice = number =>
   parseFloat((number || 0).toString().replace(/,/g, '.').replace(' ', ''))
 
 export const formatPrice = number =>
-  new Intl.NumberFormat('sk-SK', { minimumFractionDigits: 2 }).format((number || 0))
+  new Intl.NumberFormat('sk-SK', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format((number || 0))
